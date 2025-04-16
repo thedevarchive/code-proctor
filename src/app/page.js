b52724826b3f7a387cac0663a01222d134282470
Alyssa Ngo <amaengo@protonmail.com>
2025-04-16 12:17:12 +1000

    Edited text styling

--- src/app/page.js
+++ src/app/page.js
@@ -8,14 +8,14 @@ export default function Home() {
             {/* Quote at the top */}
             <div className='flex items-center justify-center bg-[url(http://3.bp.blogspot.com/-0c89sOKM9vM/VAQ-VzXjT5I/AAAAAAAAPQE/6Ksz7Osrh98/s1600/26.%2BSchlegeis%2BLake%2C%2BTyrol%2C%2BAustria%2B-%2B29%2BWonderful%2BPaths.jpg)] bg-no-repeat bg-cover bg-center  w-full h-50'>
                 <div className="text-center mb-8 bg-gray-900/80 p-2 mt-6">
-                    <p className="italic text-sm">"The journey of a thousand lines of code begins with a single 'Hello World'."</p>
+                    <p className="italic text-lg">"The journey of a thousand lines of code begins with a single '<strong className='font-bold'>Hello World</strong>'."</p>
                 </div>
             </div>
 
             <div className='pt-15 pl-8 text-left bg-[url(https://files.benramsey.com/ws/blog/2017-07-13-phptestfest/banner-1500x630.jpg)] bg-no-repeat bg-cover bg-center  w-full h-75'>
-                <div className='bg-gray-900/6D0'>
+                <div className='bg-gray-900/60'>
                 <h1 className="text-5xl font-bold mb-4">Welcome to CodeProctor</h1>
-                <h2 className="text-xl font-light mb-8">A Developer’s Playground where Code meets Precision</h2>
+                <h2 className="text-xl font-light mb-8"><em>A Developer’s Playground where <strong className='font-bold'>Code</strong> meets <strong className='font-bold'>Precision</strong></em></h2>
                 <br /> 
                 <p className="text-lg">We help you manage your workflow with tools that boost your productivity. Whether you’re debugging, building or designing, we've got your back.</p>
                 </div>
