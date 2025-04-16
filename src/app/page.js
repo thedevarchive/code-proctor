956ea47e7f9ccbb32c309d7aa6aec4f79269d834
Alyssa Ngo <amaengo@protonmail.com>
2025-04-16 12:33:51 +1000

    Added some emojis to the footer

--- src/app/page.js
+++ src/app/page.js
@@ -40,7 +40,8 @@ export default function Home() {
             </div>
 
             {/* Footer */}
-            <footer className="text-center text-sm w-full">
+            <footer className="text-center text-sm w-full p-4">
+                <p className='text-xl'>☕️ · 🤖 · 📚</p>
                 <p>Made with Heaps of Coffee, ChatGPT, and most importantly, Learning and MERNing</p>
             </footer>
         </div>
