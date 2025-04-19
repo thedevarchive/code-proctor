c2b01f6fa7ffa13d0b9841e148aa685d54709b19
Alyssa Ngo <amaengo@protonmail.com>
2025-04-19 21:30:15 +1000

    Added active states to loginheader so user would know which page they are on

--- src/app/components/headers/LogoutHeader.js
+++ src/app/components/headers/LogoutHeader.js
@@ -1,24 +1,31 @@
 "use client";
 
 import Link from "next/link"; //might use later for other pages 
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 const LogoutHeader = () => {
     const router = useRouter(); // Initialize useRouter
+    const pathname = usePathname();
 
     const signIn = () => {
         console.log("Navigating to auth…");
 
         // Navigate to the auth page
-        router.push("/auth"); 
+        router.push("/auth");
     };
 
     return (
         <div className="flex justify-between mx-4 my-4 gap-8">
             <div className="text-3xl font-bold text-green-400"><Link href="/">CodeProctor</Link></div>
             <div className="flex gap-16 mt-2 font-bold text-xl mr-12">
-                <Link href="/about">About</Link>
-                <Link href="/testimonials">Testimonials</Link>
+                <Link href="/about"
+                    className={pathname === "/about" ? "text-white" : "text-green-400"}>
+                    About
+                </Link>
+                <Link href="/testimonials"
+                    className={pathname === "/testimonials" ? "text-white" : "text-green-400"}>
+                    Testimonials
+                </Link>
             </div>
             <button
                 onClick={signIn}
